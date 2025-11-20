8bc3fd6ddac3efae008fd55847c07f6b370617c4
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-11-20 12:43:50 +0600

    feat(landing): add new section to landing page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,24 +5,30 @@ import logo from "@/public/Hotel.png";
 
 const Navbar = () => {
   return (
-    <header className="w-full bg-white shadow-md fixed top-0 z-50">
+    <header className="w-full bg-white  shadow fixed top-0 z-50 border-b border-b-gray-300">
       <nav className="container mx-auto flex items-center justify-between py-3 ">
         {/* Logo */}
-        <Link href="/" className="flex items-center">
-          <Image 
+        <Link href="/" className="flex items-center space-x-2">
+          <Image
             src={logo}
-            alt="Stay Swift Logo" 
-            width={50} 
-            height={50} 
+            alt="Stay Swift Logo"
+            width={50}
+            height={50}
             priority
             className="h-14 w-auto"
           />
+          <h3 className="text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-500">
+            Stay Swift
+          </h3>
         </Link>
 
         {/* Navigation Links */}
         <ul className="flex space-x-6 items-center text-gray-700 font-medium">
           <li>
-            <Link href="#recommended" className="hover:text-blue-600 transition">
+            <Link
+              href="#recommended"
+              className="hover:text-blue-600 transition"
+            >
               Recommended Places
             </Link>
           </li>
